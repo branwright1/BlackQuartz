a96355eb94fcb4f4c384a8a3a219f0091149d5f7
HeavyRain266 <ignacy423@gmail.com>
2021-06-03 22:54:42 +0100

    Build: fix build.zig

--- shell.nix
+++
@@ -1,7 +0,0 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation rec {
-  name = "RoseQuartz-env";
-  
-  buildInputs = with pkgs; [ wlroots libudev libevdev pixman libGL xorg.libX11 libxkbcommon ];
-  nativeBuildInputs = with pkgs; [ zig wayland wayland-protocols pkgconfig ];
-}
